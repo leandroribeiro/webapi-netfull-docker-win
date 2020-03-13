6543743b442164cad88e2f19f54891889c4c78cc
Leandro Ribeiro <leandro.ribeiro@confitec.com.br>
2020-03-13 18:19:47 -0300

    Removendo projeto Frontend MVC

--- Lab.NETFullwithDocker/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Lab.NETFullwithDocker.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Lab.NETFullwithDocker/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Lab.NETFullwithDocker.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
